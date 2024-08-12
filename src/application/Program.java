8c6bee37f503122ec07e4d81d8199a162dc49a98
Daniel Monteiro <143518947+DanielMonteiroo@users.noreply.github.com>
2024-08-12 16:35:31 -0300

    Implementação FindByDepartamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.VendedorDao;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class Program {
@@ -12,6 +15,14 @@ public class Program {
 		Vendedor vendedor = vendedorDao.findById(2);
 		System.out.println("---- TESTE FIND BY ID ----\n");
 		System.out.println(vendedor);
+		
+		System.out.println("\n---- TESTE FIND BY DEPARTAMENTO ----\n");
+		Departamento departamento = new Departamento(2, null);
+		List<Vendedor> lista = vendedorDao.findByDepartmento(departamento);
+		
+		for(Vendedor objVendedor: lista) {
+			System.out.println(objVendedor);
+		}
 	}
 
 }
--- src/model/dao/impl/VendedorDaoJDBC.java
+++ src/model/dao/impl/VendedorDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -99,6 +102,52 @@ public class VendedorDaoJDBC implements VendedorDao {
 	public List<Vendedor> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+
+	//Metodo busca por departamento
+	@Override
+	public List<Vendedor> findByDepartmento(Departamento departamento) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name AS DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+					
+			st.setInt(1, departamento.getId());
+			rs = st.executeQuery();
+			
+			List<Vendedor> lista = new ArrayList<>();
+			Map<Integer, Departamento> map = new HashMap<>();
+			
+			while(rs.next()){
+				
+				//Salvando departamento para não repetição na busca
+				Departamento dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instanciaDepartamento(rs);
+					dep = map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Vendedor vendedor = instanciaVendedor(rs,dep);
+			    lista.add(vendedor);
+			}
+			return lista;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	
